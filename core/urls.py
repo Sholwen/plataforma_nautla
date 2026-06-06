e2afb2f06919acbb73a42ef28a1479dc30182cae
Owen Tyrone Leguízamo Miranda <owenleguizamomiranda@gmail.com>
2026-06-05 19:21:13 -0600

    Creación de la app 'base', y se reubicó la vista principal (index)

--- core/urls.py
+++ core/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from . import views
+from base.views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.inicio, name='inicio'),
+    path('', inicio, name='inicio'),
 ]
