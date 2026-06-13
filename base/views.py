dab880679c90d7eda63056429493a6ff89cf793c
Owen Tyrone Leguízamo Miranda <owenleguizamomiranda@gmail.com>
2026-06-12 18:38:59 -0600

    Feat: Diseño responsivo, modal y carrusel en Vanilla JS para vista de eventos

--- base/views.py
+++ base/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'index.html')
+
+def lista_eventos(request):
+    return render(request, 'lista_eventos.html')
+
+def detalle_evento(request):
+    return render(request, 'detalle_evento.html')
